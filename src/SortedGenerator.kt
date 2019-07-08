741fd7edd85d7c933c015a7fed8bf6f7ef705a1f
Khimich Edgar <green_apple@ukr.net>
2019-07-07 22:42:28 -0400

    added bubble sort

--- src/SortedGenerator.kt
+++ src/SortedGenerator.kt
@@ -1,10 +1,13 @@
 import java.sql.Time
 import kotlin.random.Random
 import array.selectionSort
+import array.bubbleSort
 
 fun main() {
 	val array = Array<Int>(1_000_0, { (-1000..100000).random() })
 		
+	val tempArray = arrayOf(1, -24, 11, 32, -5, 26)
+	bubbleSort(array)
 	selectionSort(array)
 }
 
--- src/array/ArrayManager.kt
+++ src/array/ArrayManager.kt
@@ -1,5 +1,21 @@
 package array
 
+fun bubbleSort(array: Array<Int>) {
+	val start = System.currentTimeMillis()
+
+	for (x in 0..array.size) {
+			for (y in x downTo 0) {
+				if (y + 1 < x) {
+				if (array[y] > array[y + 1]) {
+					array[y] = array[y + 1].also { array[y + 1] = array[y] }
+				}
+			}
+		}
+	}
+	val end = System.currentTimeMillis()
+	Utils.displaySortDetails("Bubble sort", array.size, end - start)
+}
+
 fun selectionSort(array: Array<Int>) {
 	val start = System.currentTimeMillis()
 	var largestIndex: Int
