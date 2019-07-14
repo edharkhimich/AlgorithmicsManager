a406856ce6d2a2eaee6c33f97099daad6230374b
Khimich Edgar <green_apple@ukr.net>
2019-07-13 23:57:55 -0400

    add insert sort

--- src/SortedGenerator.kt
+++ src/SortedGenerator.kt
@@ -1,14 +1,19 @@
 import java.sql.Time
 import kotlin.random.Random
-import array.selectionSort
-import array.bubbleSort
+import array.ArrayManager.insertedSort
+import array.ArrayManager.bubbleSort
+import array.ArrayManager.selectionSort
 
 fun main() {
 	val array = Array<Int>(1_000_0, { (-1000..100000).random() })
-		
-	val tempArray = arrayOf(1, -24, 11, 32, -5, 26)
+
+	val tempArray = arrayOf(1, -24, 11, 32, -5, 26, 3, -44, 21, 32)
+
 	bubbleSort(array)
 	selectionSort(array)
+	insertedSort(array)
+
+
 }
 
 
--- src/array/ArrayManager.kt
+++ src/array/ArrayManager.kt
@@ -1,37 +1,62 @@
 package array
 
-fun bubbleSort(array: Array<Int>) {
-	val start = System.currentTimeMillis()
+object ArrayManager {
+	fun bubbleSort(array: Array<Int>) {
+		val start = System.currentTimeMillis()
 
-	for (x in 0..array.size) {
+		for (x in 0..array.size) {
 			for (y in x downTo 0) {
 				if (y + 1 < x) {
-				if (array[y] > array[y + 1]) {
-					array[y] = array[y + 1].also { array[y + 1] = array[y] }
+					if (array[y] > array[y + 1]) {
+						array[y] = array[y + 1].also { array[y + 1] = array[y] }
+					}
 				}
 			}
 		}
+		val end = System.currentTimeMillis()
+		Utils.displaySortDetails("Bubble sort", array.size, end - start)
 	}
-	val end = System.currentTimeMillis()
-	Utils.displaySortDetails("Bubble sort", array.size, end - start)
-}
 
-fun selectionSort(array: Array<Int>) {
-	val start = System.currentTimeMillis()
-	var largestIndex: Int
+	fun selectionSort(array: Array<Int>) {
+		val start = System.currentTimeMillis()
+		var largestIndex: Int
 
-	for (lx in array.size - 1 downTo 0) {
-		for (n in 1..lx) {
-			largestIndex = 0
-			if (array[n] > array[largestIndex]) {
-				largestIndex = n
+		for (lx in array.size - 1 downTo 0) {
+			for (n in 1..lx) {
+				largestIndex = 0
+				if (array[n] > array[largestIndex]) {
+					largestIndex = n
+				}
+				array[lx] = array[largestIndex]
+					.also { array[largestIndex] = array[lx] }
 			}
-			array[lx] = array[largestIndex]
-				.also { array[largestIndex] = array[lx] }
 		}
+		val end = System.currentTimeMillis()
+
+		Utils.displaySortDetails("Selection sort", array.size, end - start)
 	}
-	val end = System.currentTimeMillis()
 
-	Utils.displaySortDetails("Selection sort", array.size, end - start)
+	fun insertedSort(array: Array<Int>) {
+		val start = System.currentTimeMillis()
+		var newElement: Int
+
+		if (array.size > 0) {
+			var firstUnsortedIndex: Int = 1
+			for (firstUnsorted in firstUnsortedIndex until array.size) {
+				newElement = array[firstUnsorted]
+
+				for (i in firstUnsorted downTo 0) {
+					if (i > 0 && array[i - 1] > newElement) {
+						array[i] = array[i - 1]
+					} else if (i > 0 && array[i - 1] < newElement || i == 0) {
+						array[i] = newElement
+						break
+					}
+				}
+			}
+		}
+		val end = System.currentTimeMillis()
+		
+		Utils.displaySortDetails("Inserted sort", array.size, end - start)
+	}
 }
-
