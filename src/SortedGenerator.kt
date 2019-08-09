b2cbe3546e797e27f4878c461a7fdad514560dec
Khimich Edgar <green_apple@ukr.net>
2019-08-09 00:18:27 -0400

    Add count sort

--- src/SortedGenerator.kt
+++ src/SortedGenerator.kt
@@ -2,25 +2,24 @@ import java.sql.Time
 import kotlin.random.Random
 import array.ArrayManager.insertedSort
 import array.ArrayManager.bubbleSort
+import array.ArrayManager.countSort
 import array.ArrayManager.selectionSort
 import array.ArrayManager.shellSort
 import array.ArrayManager.mergeSort
 
 fun main() {
-	var array = Array<Int>(1_000_0, { (-1000..100000).random() })
+	var array = Array<Int>(1_000_0) { (-1000..100000).random() }
 
-	val tempArray = arrayOf(20, 35, -15, 7, 55, 1, 22)
+	val tempArray = arrayOf(5, 3, 2, 3, 4, 6, 9, 3, 1)
 
-	mergeSort(array, 0, array.size)
-//	array = Array<Int>(1_000_0, { (-1000..100000).random() })
-	bubbleSort(array)
-//	array = Array<Int>(1_000_0, { (-1000..100000).random() })
-	selectionSort(array)
-//	array = Array<Int>(1_000_0, { (-1000..100000).random() })
-	insertedSort(array)
-//	array = Array<Int>(1_000_0, { (-1000..100000).random() })
-	shellSort(array)
-//	array = Array<Int>(1_000_0, { (-1000..100000).random() })
+//	mergeSort(array, 0, array.size)
+//	bubbleSort(array)
+//	selectionSort(array)
+//	insertedSort(array)
+//	shellSort(array)
+//	countSort(tempArray)
+
+	tempArray.forEach { print("$it / ") }
 
 
 }
--- src/array/ArrayManager.kt
+++ src/array/ArrayManager.kt
@@ -1,132 +1,156 @@
 package array
 
 object ArrayManager {
-	fun bubbleSort(array: Array<Int>) {
-		val start = System.currentTimeMillis()
+    fun bubbleSort(array: Array<Int>) {
+        val start = System.currentTimeMillis()
 
-		for (x in 0..array.size) {
-			for (y in x downTo 0) {
-				if (y + 1 < x) {
-					if (array[y] > array[y + 1]) {
-						array[y] = array[y + 1].also { array[y + 1] = array[y] }
-					}
-				}
-			}
-		}
-		val end = System.currentTimeMillis()
-		Utils.displaySortDetails("Bubble sort", array.size, end - start)
-	}
+        for (x in 0..array.size) {
+            for (y in x downTo 0) {
+                if (y + 1 < x) {
+                    if (array[y] > array[y + 1]) {
+                        array[y] = array[y + 1].also { array[y + 1] = array[y] }
+                    }
+                }
+            }
+        }
+        val end = System.currentTimeMillis()
+        Utils.displaySortDetails("Bubble sort", array.size, end - start)
+    }
 
-	fun selectionSort(array: Array<Int>) {
-		val start = System.currentTimeMillis()
-		var largestIndex: Int
+    fun selectionSort(array: Array<Int>) {
+        val start = System.currentTimeMillis()
+        var largestIndex: Int
 
-		for (lx in array.size - 1 downTo 0) {
-			for (n in 1..lx) {
-				largestIndex = 0
-				if (array[n] > array[largestIndex]) {
-					largestIndex = n
-				}
-				array[lx] = array[largestIndex]
-					.also { array[largestIndex] = array[lx] }
-			}
-		}
-		val end = System.currentTimeMillis()
+        for (lx in array.size - 1 downTo 0) {
+            for (n in 1..lx) {
+                largestIndex = 0
+                if (array[n] > array[largestIndex]) {
+                    largestIndex = n
+                }
+                array[lx] = array[largestIndex]
+                        .also { array[largestIndex] = array[lx] }
+            }
+        }
+        val end = System.currentTimeMillis()
 
-		Utils.displaySortDetails("Selection sort", array.size, end - start)
-	}
+        Utils.displaySortDetails("Selection sort", array.size, end - start)
+    }
 
-	fun insertedSort(array: Array<Int>) {
-		val start = System.currentTimeMillis()
-		var newElement: Int
+    fun insertedSort(array: Array<Int>) {
+        val start = System.currentTimeMillis()
+        var newElement: Int
 
-		if (array.size > 0) {
-			var firstUnsortedIndex: Int = 1
-			for (firstUnsorted in firstUnsortedIndex until array.size) {
-				newElement = array[firstUnsorted]
+        if (array.size > 0) {
+            var firstUnsortedIndex: Int = 1
+            for (firstUnsorted in firstUnsortedIndex until array.size) {
+                newElement = array[firstUnsorted]
 
-				for (i in firstUnsorted downTo 0) {
-					if (i > 0 && array[i - 1] > newElement) {
-						array[i] = array[i - 1]
-					} else if (i > 0 && array[i - 1] < newElement || i == 0) {
-						array[i] = newElement
-						break
-					}
-				}
-			}
-		}
-		val end = System.currentTimeMillis()
+                for (i in firstUnsorted downTo 0) {
+                    if (i > 0 && array[i - 1] > newElement) {
+                        array[i] = array[i - 1]
+                    } else if (i > 0 && array[i - 1] < newElement || i == 0) {
+                        array[i] = newElement
+                        break
+                    }
+                }
+            }
+        }
+        val end = System.currentTimeMillis()
 
-		Utils.displaySortDetails("Inserted sort", array.size, end - start)
-	}
+        Utils.displaySortDetails("Inserted sort", array.size, end - start)
+    }
 
-	fun shellSort(array: Array<Int>) {
-		val start = System.currentTimeMillis()
+    fun shellSort(array: Array<Int>) {
+        val start = System.currentTimeMillis()
 
-		var gap = array.size / 2
-		var j: Int
-		var temp: Int
+        var gap = array.size / 2
+        var j: Int
+        var temp: Int
 
-		while (gap > 0) {
+        while (gap > 0) {
 
-			var i = gap
+            var i = gap
 
-			while (i < array.size) {
-				temp = array[i]
+            while (i < array.size) {
+                temp = array[i]
 
-				j = i
+                j = i
 
-				while (j >= gap && array[j - gap] > temp) {
-					array[j] = array[j - gap]
-					j -= gap
-				}
+                while (j >= gap && array[j - gap] > temp) {
+                    array[j] = array[j - gap]
+                    j -= gap
+                }
 
-				array[j] = temp
-				i += 1
-			}
-			gap /= 2
-		}
+                array[j] = temp
+                i += 1
+            }
+            gap /= 2
+        }
 
-		val end = System.currentTimeMillis()
-		Utils.displaySortDetails("Shell sort", array.size, end - start)
+        val end = System.currentTimeMillis()
+        Utils.displaySortDetails("Shell sort", array.size, end - start)
 
-	}
-	
-	fun mergeSort(array: Array<Int>, start: Int, end: Int){
-		val s = System.currentTimeMillis()
-		mergeSortImpl(array, start, end)
-		val end = System.currentTimeMillis()
-		Utils.displaySortDetails("Merge sort", array.size, end - s)
-	}
+    }
 
-	fun mergeSortImpl(arr: Array<Int>, start: Int, end: Int) {
+    fun mergeSort(array: Array<Int>, start: Int, end: Int) {
+        val s = System.currentTimeMillis()
+        mergeSortImpl(array, start, end)
+        val e = System.currentTimeMillis()
+        Utils.displaySortDetails("Merge sort", array.size, e - s)
+    }
 
-		if (end - start < 2) return
+    fun mergeSortImpl(arr: Array<Int>, start: Int, end: Int) {
 
-		val mid = (start + end) / 2
+        if (end - start < 2) return
 
-		mergeSortImpl(arr, start, mid)
-		mergeSortImpl(arr, mid, end)
-		merge(arr, start, mid, end)
-	}
+        val mid = (start + end) / 2
 
-	private fun merge(arr: Array<Int>, start: Int, mid: Int, end: Int) {
+        mergeSortImpl(arr, start, mid)
+        mergeSortImpl(arr, mid, end)
+        merge(arr, start, mid, end)
+    }
 
-		if (arr[mid - 1] <= arr[mid]) return
+    private fun merge(arr: Array<Int>, start: Int, mid: Int, end: Int) {
 
-		var i = start
-		var j = mid
-		var tempIndex = 0
+        if (arr[mid - 1] <= arr[mid]) return
 
-		var tempArray = Array<Int>(end - start, {i -> i})
-		
+        var i = start
+        var j = mid
+        var tempIndex = 0
 
-		while (i < mid && j < end) {
-			tempArray[tempIndex++] = if (arr[i] <= arr[j]) arr[i++] else arr[j++]
-		}
+        var tempArray = Array<Int>(end - start, { i -> i })
 
-		System.arraycopy(arr, i, arr, start + tempIndex, mid - i)
-		System.arraycopy(tempArray, 0, arr, start, tempIndex)
 
-	}
+        while (i < mid && j < end) {
+            tempArray[tempIndex++] = if (arr[i] <= arr[j]) arr[i++] else arr[j++]
+        }
+
+        System.arraycopy(arr, i, arr, start + tempIndex, mid - i)
+        System.arraycopy(tempArray, 0, arr, start, tempIndex)
+
+    }
+
+    /** This algorithm is better to use when we have not a huge range of Integers.
+	 * The type should be only an Int. */
+    fun countSort(arr: Array<Int>) {
+        if (arr.isEmpty()) return
+        arr.max()?.let { max ->
+            println("maxValue = $max")
+
+            val spacingArray = Array(max) { 0 }
+
+            arr.forEach { i -> spacingArray[i - 1] += 1 }
+
+            var counter = 0
+            spacingArray.forEachIndexed { index, i ->
+                if (i > 0) {
+                    for (x in 0 until i) {
+                        arr[counter] = index + 1
+                        counter++
+                    }
+                }
+            }
+        }
+    }
+
 }
